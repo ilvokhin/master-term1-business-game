1f95e75fc2651395adfa1b2984ef060cff9d5e82
Dima Ilvokhin <r3tsky@gmail.com>
2015-11-28 16:06:31 +0300

    init database scheme
    - add init db script
    - describe few db elems

--- app/database.py
+++ app/database.py
@@ -1,2 +1,41 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*
+
+from couchdbkit import loaders
+from couchdbkit import Server
+from couchdbkit import Document
+from couchdbkit import StringProperty
+from couchdbkit import SetProperty
+from couchdbkit import DateTimeProperty
+
+def connect_db(db_name):
+  srv = Server()
+  return srv.get_or_create_db(db_name)
+
+def init_db(db_name):
+  db = connect_db(db_name)
+  loader = loaders.FileSystemDocsLoader('app/_design')
+  loader.sync(db, verbose = True)
+
+class User(Document):
+  username = StringProperty()
+  salt = StringProperty()
+  password = StringProperty()
+
+class Task(Document):
+  author = StringProperty()
+  assigned = StringProperty()
+  priority = StringProperty()
+  title = StringProperty()
+  text = StringProperty()
+  tags = SetProperty()
+  status = StringProperty()
+  project = StringProperty()
+  comments = SetProperty()
+  create_date = DateTimeProperty()
+  update_date = DateTimeProperty()
+
+class Comment(Document):
+  author = StringProperty()
+  text = StringProperty()
+  date = DateTimeProperty()
--- reinit_db.py
+++ reinit_db.py
@@ -1,2 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*
+
+from app import app
+from app.database import init_db
+
+init_db(app.config.get('DB'))
